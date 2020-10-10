d42ec62cba89b41b5b475d142aaf6f84ad37908b
DayDay <daydaybigking@sina.com>
2020-10-10 09:25:41 +0800

    初步将旧的 config 拆分成 dev 和 prod

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -3,24 +3,13 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "production",
   entry: {
     app: "./src/index.js",
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: "./dist",
-    hot: true,
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({ title: "Output Management" }),
   ],
-  optimization: {
-    splitChunks: {
-      chunks: "all",
-    },
-  },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
